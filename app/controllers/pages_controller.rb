802964403ba7aba0c57eab721d2f9d29f7b36290
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-04-02 12:17:32 +0200

    chore: ran webpack, created routes for home, about, projects, skills, contact, blog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def projects
+  end
+
+  def skills
+  end
+
+  def contact
+  end
+
+  def blog
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/projects', to: 'pages#projects'
+  get '/skills', to: 'pages#skills'
+  get '/contact', to: 'pages#contact'
+  get '/blog', to: 'pages#blog'
 end
