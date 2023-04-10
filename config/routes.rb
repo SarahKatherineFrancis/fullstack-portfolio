c296d6ecea4167bb2a5e5337360b700c47688c5e
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-04-10 21:10:45 +0200

    chore: changed pages to partials to allow for navbar to nav to sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get '/about', to: 'pages#about'
-  get '/projects', to: 'pages#projects'
-  get '/skills', to: 'pages#skills'
-  get '/education', to: 'pages#education'
-  get '/contact', to: 'pages#contact'
-  get '/blog', to: 'pages#blog'
+  get '/resume', to: 'pages#resume'
 end
